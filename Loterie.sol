d87984e4a8fa6f79e1ffa9d9cd09336ac9c6a254
hackheera <rmichon@telecom-paristech.fr>
2019-03-22 23:46:33 +0100

    same value for every bet

--- Loterie.sol
+++ Loterie.sol
@@ -4,17 +4,14 @@ pragma solidity ^0.5.0;
 contract Loterie { 
 
 	// -------------------- variables -----------------------------------------	
-	uint public lastRoundTimestamp;
-	uint public nextRoundTimestamp; 
-	uint _interval; 
 	uint _bloc_cible; // le bloc sur le nonce duquel les participants vont parier
 	address _organisation; //l'organisation de la loterie
 	address _beneficiaire; //l'ONG bénéficiant de la loterie
+	uint _mise; //tout le monde mise autant, en wei
 
 	struct Pari { 
 		address payable participant; 
 		uint nonce_devine; 
-		uint mise; 
 	}
 	Pari[] public paris;
 
@@ -22,11 +19,11 @@ contract Loterie {
 	// fin des variables
 
 	// -------------------- constructeur --------------------------------------
-	// ici encore la syntaxe de la doc est dépréciée, il faut utiliser le constructeur prévu ("constructor(...) { ... }")
-	constructor(uint bloc_cible) public { 
+	constructor(uint bloc_cible, address beneficiaire, uint mise) public { 
 		_bloc_cible = bloc_cible; 
+		_beneficiaire = beneficiaire;
 		_organisation = msg.sender; //msg est le message qui appelle le smart contract
-		//nextRoundTimestamp = now + _interval; //à calculer 
+		_mise = mise;
 	}
 
 
@@ -35,57 +32,25 @@ contract Loterie {
 	// le underscore est un opérateur qui symbolise le code de la fonction modifiée
 	// équivalent : si il y n'y a pas d'ether dans la TX require(false) ==> throw (exception), else do ...
     modifier transactionMustContainEther() {
-        if (msg.value == 0) require(false);
+        if (msg.value != _mise) require(false);
         _ ;
     }
-	// contrairement à ce qui est indiqué dans la doc, il faut mettre un ";" après l'underscore
-
-	// dans le business model de la Loterie cette méthode n'est pas pertinente
-	// parcourt les paris en cours pour calculer la somme maximale que la banque devra payer si tous les joueurs gagnent
-	// modifier bankMustBeAbleToPayForPariType(PariType betType) { 
-	// 	uint necessaryBalance = 0; 
-	// 	for (uint i = 0; i < paris.length; i++) { 
-	// 		necessaryBalance += paris[i].mise; 
-	// 	}
-	// 	necessaryBalance += msg.mise;
-	// 	if (necessaryBalance > address(this).balance) require(false); 
-	// 	_ ;
-	// }
-	// contrairement à ce qui est indiqué dans la doc, il faut mettre un ";" après l'underscore
 
 
 	// -------------------- parier -----------------------------------------
-	// plein de modifiers qui vérifient la validité de l'appel
-	/*function betSingle(uint number) public payable transactionMustContainEther() {
-		if (number > 36) require(false); // arrête l'éxécution si pb
-		paris.push(Pari({
-			 betType: PariType.Single, participant: msg.sender, number: number, mise: msg.value 
-		})); 		// parcourt les paris de type single (sur un chiffre) et les ajoute au tableau paris
-	}
-
-	function betEven() public payable transactionMustContainEther() {
-		paris.push(Pari({
-			betType: PariType.Even, participant: msg.sender, number: 0, mise: msg.value 
-		}));
-	}*/
-
-	// parier
 	// TODO : nonce sur lequel il parie
+	// TODO : mettre des mises personnalisées
 	function parier() public payable transactionMustContainEther(){
 		paris.push(Pari({
 			//TODO : verifier le cast & la mise en forme de msg data
-			participant: msg.sender, nonce_devine: (uint)msg.data, mise: msg.value 
+			participant: msg.sender, nonce_devine: (uint)msg.data /*, mise: msg.value*/ 
 		}));
 	}
 
 	// --------------------- sorte d'API mais dans la Blockchain -----------------
 	// retourne le nombre de paris et la valeur totale des gains
 	function getParisCountAndValue() public view returns(uint, uint) {
-		uint gain = 0;
-		for (uint i = 0; i < paris.length; i++) {
-			gain += paris[i].mise;
-		}
-		return (paris.length, gain);
+		return (paris.length, (uint)(paris.length * _mise);
 	}
 
 
@@ -93,26 +58,25 @@ contract Loterie {
 
 	function launch() public {
 		if (block.number < _bloc_cible) require(false);
+		(uint len, uint gain) = getParisCountAndValue();
 
 		uint nonce = uint(blockhash(_bloc_cible)); 
 		// TODO : utilser le nonce au lieu du hash
 		// TODO : on ne considère que les derniers chiffres
-		uint gain;
 		uint winner = 0;
 		uint nonce_diff;
 		uint nonce_min = 5000; // arbitrary high value, max of nonce values
-		for (uint i = 0; i < paris.length; i++){
+		// on parcourt tous les paris --> prix en gaz en O(n) ???
+		for (uint i = 0; i < len; i++){
 			nonce_diff = uint(paris.nonce_devine - nonce) // toujours positif
 			if(nonce_diff < nonce_min){
 				winner = i;
 			}
 		}
-		// on parcourt tous les paris --> prix en gaz ??
 		// TODO : vérifier que c'est bien l'ordre chronologique
 		paris[winner].participant.transfer(gains * 0.3); // rewarde le vainqueur
 		_organisation.transfer(gains * 0.1); // reward l'_organisation
 		_beneficiaire.transfer(gains * 0.6); // reward le bénéficiaire
-
 	}
 
 }
